af721518d3c577823cdbebc7a0ba013fa7d71031
YonasAdane <yonasadane94@gmail.com>
2024-10-20 16:47:49 +0300

    added https to api docs

--- src/utils/swagger/swagger.js
+++ src/utils/swagger/swagger.js
@@ -2,12 +2,13 @@ import swaggerAutogen from 'swagger-autogen';
 const doc = {
   info: {
     title: 'ASTU Football Cup API',
-    description: 'Description'
+    description: 'ASTU Football Cup is designed for managing football clubs, match schedules, and real-time match updates during the university football cup.\n The platform is built to provide administrators with the ability to manage clubs, players, matches, and reports while allowing secure access to different user roles.'
   },
-  host: 'https://astu-football-cup-api.onrender.com'
+  host: 'astu-football-cup-api.onrender.com',
+  schemes: ['https'],
 };
 
 const outputFile = './swagger-output.json';
 const routes = ["../../index.js"];
-
-swaggerAutogen(outputFile, routes, doc);
+ 
+swaggerAutogen(outputFile, routes, doc); 
